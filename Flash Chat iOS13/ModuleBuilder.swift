30c8126fe5403fdd55b4c457220e4dad072d639f
Ilia Shapovalov <greateman@mail.ru>
2022-07-21 22:05:31 +0300

    Create develop branch. Starting to develop new feature.

---
+++ Flash Chat iOS13/ModuleBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  ModuleBuilder.swift
+//  Flash Chat iOS13
+//
+//  Created by Илья Шаповалов on 21.07.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+protocol ModuleBuilderProtocol {
+    static func createWelcomeVC() -> UIViewController
+}
